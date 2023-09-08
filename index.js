11df7647482d169292fe15dc23360f6cf2f074d5
Thawan Lucas <thawanlucascaze@hotmail.com>
2023-09-07 21:23:17 -0300

    New features

---
+++ index.js
@@ -0,0 +1,125 @@
+var altura = 0
+var largura = 0
+var vidas = 1
+var tempo = 60
+var criaMosquitoTempo = 1500
+
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+// NIVEL
+if(nivel === 'normal') {
+    //1500
+    criaMosquitoTempo = 1500
+} else if(nivel === 'dificil') {
+    //1000
+    criaMosquitoTempo = 1000
+} else if(nivel === 'renata') {
+    //750
+    criaMosquitoTempo = 750
+}
+
+
+var cronometro = setInterval(function() {
+    tempo -= 1
+
+    if(tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(criarMosca)
+        //VITORIA DO JOGO
+        window.location.href = 'vitoria.html'
+    } else {
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+    
+}, 1000);
+
+function ajustaTamanho() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(largura, altura)
+}
+
+ajustaTamanho()
+
+function posicaoRandomica() {
+
+    //REMOVE O MOSQUITO E AFETA AS VIDAS
+    if(document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+
+        //FIM DE JOGO
+        if(vidas > 3) {
+            window.location.href = 'fimjogo.html'
+        } else {
+            document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png'
+            vidas++
+        }
+
+        
+
+    }
+
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    console.log('X: ' + posicaoX + ' Y: ' + posicaoY)
+
+    // CRIA O MOSQUITO
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = tamanhoAleatorio() + ' ' +  ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove()
+    }
+
+    document.body.appendChild(mosquito)
+}
+
+posicaoRandomica()
+
+function tamanhoAleatorio () {
+    var classe = Math.floor(Math.random() * 3)
+    
+    switch(classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+    
+}
+
+function ladoAleatorio () {
+    var classe = Math.floor(Math.random() * 2)
+    
+    switch(classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
+
+}
+
+
+ /* TEMPO DE QUANDO AS MOSCAS VAO NASCER */
+
+var criarMosca = setInterval(function () {
+    posicaoRandomica()
+}, criaMosquitoTempo)
+
+
+
